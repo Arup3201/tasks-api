366a8dbfaf5c56d47a1d7678aac25ec65fe0ad06
Arup3201 <arupjana7365@gmail.com>
2025-08-22 21:57:24 +0530

    feat: handle /tasks post request

--- main.go
+++ main.go
@@ -6,10 +6,26 @@ import (
 	"os"
 )
 
+const PORT = ":8000"
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	if r.Method!=http.MethodPost {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	value := r.FormValue("task")
+	fmt.Println(value)
+
+	fmt.Fprintf(w, "[TODO]")
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/tasks", createTask)
 
-	err := http.ListenAndServe(":8000", nil)
+	fmt.Printf("http server started at %s\n", PORT)
+	err := http.ListenAndServe(PORT, nil)
 	if err!=nil {
 		fmt.Fprintf(os.Stderr, "http listen and server failed - %s", err)
 	}
