1274b0134fda0b898f281004728d79dff0797174
Arup3201 <arupjana7365@gmail.com>
2025-08-29 19:05:54 +0530

    refactor: module name changed

--- internal/handlers/tasks.go
+++ internal/handlers/tasks.go
@@ -1,20 +1,13 @@
-package tasks
+package handlers
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 
 	"github.com/Arup3201/gotasks/internal/storage"
 )
 
 func GetTasks(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		log.Fatalf("%s method not allowed on GetTasks", r.Method)
-		return
-	}
-
 	tasks, err := storage.GetAll("data/tasks.json")
 	if err != nil {
 		http.Error(w, "Failed to open tasks.json", http.StatusInternalServerError)
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 
-	tasks "github.com/Arup3201/gotasks/internal/handlers"
+	"github.com/Arup3201/gotasks/internal/handlers"
 )
 
 const PORT = ":8000"
@@ -12,7 +12,7 @@ const PORT = ":8000"
 func main() {
 	http.HandleFunc("/tasks", func (w http.ResponseWriter, r *http.Request) {
 		if r.Method==http.MethodGet {
-			tasks.GetTasks(w, r)
+			handlers.GetTasks(w, r)
 		}
 	})
 
