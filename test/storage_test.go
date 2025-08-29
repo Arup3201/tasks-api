8a9629f261e677fa620ba094221c94a77e1f8dc0
Arup3201 <arupjana7365@gmail.com>
2025-08-29 17:01:38 +0530

    test added for storage.Add()

--- test/storage_test.go
+++ test/storage_test.go
@@ -3,12 +3,15 @@ package storage
 import (
 	"os"
 	"testing"
+	"time"
 
+	"github.com/Arup3201/gotasks/internal/models"
 	"github.com/Arup3201/gotasks/internal/storage"
 )
 
 func TestCreateStorage(t *testing.T) {
-	_, err := storage.CreateStorage("../data/test.json")
+	testFile := "../data/test.json"
+	_, err := storage.CreateStorage(testFile)
 	if err != nil {
 		t.Errorf("storage.CreateStorage failed: %v", err)
 	}
@@ -16,6 +19,34 @@ func TestCreateStorage(t *testing.T) {
 	_, err = os.Open("../data/test.json")
 	if err != nil {
 		t.Errorf("storage.CreateStorage did not create file")
+	}
+}
+
+func TestAdd(t *testing.T) {
+	testFile := "../data/test.json"
+	testTask := models.Task{
+		Id: "T001",
+		Title: "Golang practice",
+		Description: "Practice golang for 20 mins - focusing on goroutines, and testing",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	err := storage.Add(testTask, testFile)
+	if err != nil {
+		t.Errorf("storage.Add() failed to add: %v", err)
+	}
+
+	tasks, _ := storage.GetAll(testFile)
+	saved := false
+	for _, task := range tasks {
+		if task.Id == testTask.Id {
+			saved = true
+		}
+	}
+
+	if !saved {
+		t.Errorf("storage.Add() did not save")
 	}
 }
 
