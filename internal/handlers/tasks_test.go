ba08b3efa1ed4746720f60e60268efeb58866719
Arup3201 <arupjana7365@gmail.com>
2025-09-13 21:13:40 +0530

    update: testa updated and new tests added for GET task with ID

--- internal/handlers/tasks_test.go
+++ internal/handlers/tasks_test.go
@@ -46,15 +46,15 @@ func (m *MockStorage) Search(by models.FieldName, query string) ([]models.Task, 
 	return nil, nil
 }
 
-func getTestContext(t testing.TB, w http.ResponseWriter, r *http.Request) *gin.Context {
+func getTestRouter(t testing.TB) *gin.Engine {
 	t.Helper()
 
 	gin.SetMode(gin.TestMode)
 
-	ctx, _ := gin.CreateTestContext(w)
-	ctx.Request = r
+	router := gin.New()
+	router.Use(HandleErrors())
 
-	return ctx
+	return router
 }
 
 func TestGETAllTasks(t *testing.T) {
@@ -80,11 +80,12 @@ func TestGETAllTasks(t *testing.T) {
 			},
 		}
 		tHandler := NewTaskHandler(mock)
+		router := getTestRouter(t)
+		router.GET("/tasks", tHandler.GetAllTasks)
 		request, _ := http.NewRequest("GET", "/tasks", nil)
 		response := httptest.NewRecorder()
-		ctx := getTestContext(t, response, request)
 
-		tHandler.GetAllTasks(ctx)
+		router.ServeHTTP(response, request)
 
 		var got []models.Task
 		want := 2
@@ -99,11 +100,12 @@ func TestGETAllTasks(t *testing.T) {
 	t.Run("returns no tasks", func(t *testing.T) {
 		mock := &MockStorage{}
 		tHandler := NewTaskHandler(mock)
+		router := getTestRouter(t)
+		router.GET("/tasks", tHandler.GetAllTasks)
 		request, _ := http.NewRequest("GET", "/tasks", nil)
 		response := httptest.NewRecorder()
-		ctx := getTestContext(t, response, request)
 
-		tHandler.GetAllTasks(ctx)
+		router.ServeHTTP(response, request)
 
 		var got []models.Task
 		want := 0
@@ -113,6 +115,58 @@ func TestGETAllTasks(t *testing.T) {
 		}
 		if len(got) != want {
 			t.Errorf("response is wrong, expected %d tasks but got %d tasks", want, len(got))
+		}
+	})
+}
+
+func TestGetTaskWithId(t *testing.T) {
+	t.Run("returns task with id 1", func(t *testing.T) {
+		mock := &MockStorage{
+			tasks: map[string]models.Task{
+				"1": {
+					Id:          "1",
+					Title:       "Task 1",
+					Description: "No description",
+					Completed:   false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				"2": {
+					Id:          "2",
+					Title:       "Task 2",
+					Description: "No description",
+					Completed:   true,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+			},
+		}
+		tHandler := NewTaskHandler(mock)
+		router := getTestRouter(t)
+		router.GET("/tasks/:id", tHandler.GetTaskWithId)
+		request, _ := http.NewRequest("GET", "/tasks/1", nil)
+		response := httptest.NewRecorder()
+
+		router.ServeHTTP(response, request)
+
+		want := 200
+		if got := response.Result().StatusCode; got != want {
+			t.Errorf("response status is wrong, expected %d but got %d", want, got)
+		}
+	})
+	t.Run("returns task not found", func(t *testing.T) {
+		mock := &MockStorage{}
+		tHandler := NewTaskHandler(mock)
+		router := getTestRouter(t)
+		router.GET("/tasks/:id", tHandler.GetTaskWithId)
+		request, _ := http.NewRequest("GET", "/tasks/1", nil)
+		response := httptest.NewRecorder()
+
+		router.ServeHTTP(response, request)
+
+		want := 404
+		if got := response.Result().StatusCode; got != want {
+			t.Errorf("response status is wrong, expected %d but got %d", want, got)
 		}
 	})
 }
