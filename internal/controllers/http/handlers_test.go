8a1f6b05e1ba4f147b45f91fde7c6dc18a7040d6
Arup3201 <arupjana7365@gmail.com>
2025-09-22 20:16:13 +0530

    handler tests added for add, update, search

--- internal/controllers/http/handlers_test.go
+++ internal/controllers/http/handlers_test.go
@@ -195,3 +195,389 @@ func TestAddTask(t *testing.T) {
 		}
 	})
 }
+
+func TestGetTask(t *testing.T) {
+	t.Run("get task success", func(t *testing.T) {
+		repo := &MockRepository{
+			tasks: []entities.Task{
+				{
+					Id:          1,
+					Title:       "Task 1",
+					Description: "No description",
+					IsCompleted: false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				{
+					Id:          2,
+					Title:       "Task 2",
+					Description: "No description",
+					IsCompleted: true,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+			},
+		}
+		serviceHandler, _ := services.NewTaskService(repo)
+		routeHandler := getRouteHandler(serviceHandler)
+		request, _ := http.NewRequest("GET", "/tasks/2", nil)
+		response := httptest.NewRecorder()
+		ctx, engine := getTestContext(t, response, request)
+		engine.GET("/tasks/:id", routeHandler.GetTask)
+
+		engine.ServeHTTP(response, ctx.Request)
+
+		var got entities.Task
+		err := json.NewDecoder(response.Body).Decode(&got)
+		if err != nil {
+			log.Fatal("JSON decoding failed")
+		}
+		want := 2
+		if got.Id != want {
+			t.Errorf("response is wrong, expected task with ID %d but got %d tasks", want, got.Id)
+		}
+	})
+	t.Run("get task success", func(t *testing.T) {
+		repo := &MockRepository{
+			tasks: []entities.Task{
+				{
+					Id:          1,
+					Title:       "Task 1",
+					Description: "No description",
+					IsCompleted: false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				{
+					Id:          2,
+					Title:       "Task 2",
+					Description: "No description",
+					IsCompleted: true,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+			},
+		}
+		serviceHandler, _ := services.NewTaskService(repo)
+		routeHandler := getRouteHandler(serviceHandler)
+		request, _ := http.NewRequest("GET", "/tasks/3", nil)
+		response := httptest.NewRecorder()
+		ctx, engine := getTestContext(t, response, request)
+		engine.Use(middlewares.HttpErrorResponse())
+		engine.GET("/tasks/:id", routeHandler.GetTask)
+
+		engine.ServeHTTP(response, ctx.Request)
+
+		want := 404
+		if got := response.Result().StatusCode; got != want {
+			t.Errorf("should return NotFound error, expected status code %d but got %d", want, got)
+		}
+	})
+}
+
+func TestUpdateTask(t *testing.T) {
+	t.Run("update task with new title", func(t *testing.T) {
+		repo := &MockRepository{
+			tasks: []entities.Task{
+				{
+					Id:          1,
+					Title:       "Task 1",
+					Description: "No description",
+					IsCompleted: false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				{
+					Id:          2,
+					Title:       "Task 2",
+					Description: "No description",
+					IsCompleted: true,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+			},
+		}
+		serviceHandler, _ := services.NewTaskService(repo)
+		routeHandler := getRouteHandler(serviceHandler)
+		payload := strings.NewReader(`{
+			"title": "Test 2 (edited)"
+		}`)
+		request, _ := http.NewRequest("PATCH", "/tasks/2", payload)
+		response := httptest.NewRecorder()
+		ctx, engine := getTestContext(t, response, request)
+		engine.PATCH("/tasks/:id", routeHandler.UpdateTask)
+
+		engine.ServeHTTP(response, ctx.Request)
+
+		var got entities.Task
+		err := json.NewDecoder(response.Body).Decode(&got)
+		if err != nil {
+			log.Fatal("JSON decoding failed")
+		}
+		want := "Test 2 (edited)"
+		if got.Title != want {
+			t.Errorf("expected title after update %s, but got %s", want, got.Title)
+		}
+	})
+	t.Run("update task with new description", func(t *testing.T) {
+		repo := &MockRepository{
+			tasks: []entities.Task{
+				{
+					Id:          1,
+					Title:       "Task 1",
+					Description: "No description",
+					IsCompleted: false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				{
+					Id:          2,
+					Title:       "Task 2",
+					Description: "No description",
+					IsCompleted: true,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+			},
+		}
+		serviceHandler, _ := services.NewTaskService(repo)
+		routeHandler := getRouteHandler(serviceHandler)
+		payload := strings.NewReader(`{
+			"description": "Test 2 description (edited)"
+		}`)
+		request, _ := http.NewRequest("PATCH", "/tasks/2", payload)
+		response := httptest.NewRecorder()
+		ctx, engine := getTestContext(t, response, request)
+		engine.PATCH("/tasks/:id", routeHandler.UpdateTask)
+
+		engine.ServeHTTP(response, ctx.Request)
+
+		var result entities.Task
+		err := json.NewDecoder(response.Body).Decode(&result)
+		if err != nil {
+			log.Fatal("JSON decoding failed")
+		}
+		want := "Test 2 description (edited)"
+		if got := result.Description; got != want {
+			t.Errorf("expected description after update %s, but got %s", want, got)
+		}
+	})
+	t.Run("update task with new is_completed", func(t *testing.T) {
+		repo := &MockRepository{
+			tasks: []entities.Task{
+				{
+					Id:          1,
+					Title:       "Task 1",
+					Description: "No description",
+					IsCompleted: false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				{
+					Id:          2,
+					Title:       "Task 2",
+					Description: "No description",
+					IsCompleted: true,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+			},
+		}
+		serviceHandler, _ := services.NewTaskService(repo)
+		routeHandler := getRouteHandler(serviceHandler)
+		payload := strings.NewReader(`{
+			"is_completed": true
+		}`)
+		request, _ := http.NewRequest("PATCH", "/tasks/1", payload)
+		response := httptest.NewRecorder()
+		ctx, engine := getTestContext(t, response, request)
+		engine.PATCH("/tasks/:id", routeHandler.UpdateTask)
+
+		engine.ServeHTTP(response, ctx.Request)
+
+		var result entities.Task
+		err := json.NewDecoder(response.Body).Decode(&result)
+		if err != nil {
+			log.Fatal("JSON decoding failed")
+		}
+		want := true
+		if got := result.IsCompleted; got != want {
+			t.Errorf("expected description after update %t, but got %t", want, got)
+		}
+	})
+	t.Run("update task fail not found", func(t *testing.T) {
+		repo := &MockRepository{
+			tasks: []entities.Task{
+				{
+					Id:          1,
+					Title:       "Task 1",
+					Description: "No description",
+					IsCompleted: false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				{
+					Id:          2,
+					Title:       "Task 2",
+					Description: "No description",
+					IsCompleted: true,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+			},
+		}
+		serviceHandler, _ := services.NewTaskService(repo)
+		routeHandler := getRouteHandler(serviceHandler)
+		payload := strings.NewReader(`{
+			"title": "Test 3 (edited)"
+		}`)
+		request, _ := http.NewRequest("PATCH", "/tasks/3", payload)
+		response := httptest.NewRecorder()
+		ctx, engine := getTestContext(t, response, request)
+		engine.Use(middlewares.HttpErrorResponse())
+		engine.PATCH("/tasks/:id", routeHandler.UpdateTask)
+
+		engine.ServeHTTP(response, ctx.Request)
+
+		want := 404
+		if got := response.Result().StatusCode; got != want {
+			t.Errorf("expected NotFound error, expected status code %d but got %d", want, got)
+		}
+	})
+	t.Run("update task with no op", func(t *testing.T) {
+		repo := &MockRepository{
+			tasks: []entities.Task{
+				{
+					Id:          1,
+					Title:       "Task 1",
+					Description: "No description",
+					IsCompleted: false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				{
+					Id:          2,
+					Title:       "Task 2",
+					Description: "No description",
+					IsCompleted: true,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+			},
+		}
+		serviceHandler, _ := services.NewTaskService(repo)
+		routeHandler := getRouteHandler(serviceHandler)
+		payload := strings.NewReader(`{}`)
+		request, _ := http.NewRequest("PATCH", "/tasks/3", payload)
+		response := httptest.NewRecorder()
+		ctx, engine := getTestContext(t, response, request)
+		engine.Use(middlewares.HttpErrorResponse())
+		engine.PATCH("/tasks/:id", routeHandler.UpdateTask)
+
+		engine.ServeHTTP(response, ctx.Request)
+
+		want := 204
+		if got := response.Result().StatusCode; got != want {
+			t.Errorf("expected NoOp response, expected status code %d but got %d", want, got)
+		}
+	})
+}
+
+func TestSearchTasks(t *testing.T) {
+	t.Run("search tasks success", func(t *testing.T) {
+		repo := &MockRepository{
+			tasks: []entities.Task{
+				{
+					Id:          1,
+					Title:       "Task 1",
+					Description: "No description",
+					IsCompleted: false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				{
+					Id:          2,
+					Title:       "Task 2",
+					Description: "No description",
+					IsCompleted: true,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				{
+					Id:          3,
+					Title:       "Task 2",
+					Description: "Task 2 additional",
+					IsCompleted: false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+			},
+		}
+		serviceHandler, _ := services.NewTaskService(repo)
+		routeHandler := getRouteHandler(serviceHandler)
+		request, _ := http.NewRequest("GET", "/search/tasks?q=2", nil)
+		response := httptest.NewRecorder()
+		ctx, engine := getTestContext(t, response, request)
+
+		engine.GET("/search/tasks", routeHandler.SearchTasks)
+		engine.ServeHTTP(response, ctx.Request)
+
+		var got []entities.Task
+		err := json.NewDecoder(response.Body).Decode(&got)
+		if err != nil {
+			log.Fatal("JSON decoding failed")
+		}
+		want := 2
+		if len(got) != want {
+			t.Errorf("response is wrong, expected %d searched tasks but got %d tasks", want, len(got))
+		}
+	})
+	t.Run("search tasks no task found", func(t *testing.T) {
+		repo := &MockRepository{
+			tasks: []entities.Task{
+				{
+					Id:          1,
+					Title:       "Task 1",
+					Description: "No description",
+					IsCompleted: false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				{
+					Id:          2,
+					Title:       "Task 2",
+					Description: "No description",
+					IsCompleted: true,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+				{
+					Id:          3,
+					Title:       "Task 2",
+					Description: "Task 2 additional",
+					IsCompleted: false,
+					CreatedAt:   time.Now(),
+					UpdatedAt:   time.Now(),
+				},
+			},
+		}
+		serviceHandler, _ := services.NewTaskService(repo)
+		routeHandler := getRouteHandler(serviceHandler)
+		request, _ := http.NewRequest("GET", "/search/tasks?q=3", nil)
+		response := httptest.NewRecorder()
+		ctx, engine := getTestContext(t, response, request)
+
+		engine.GET("/search/tasks", routeHandler.SearchTasks)
+		engine.ServeHTTP(response, ctx.Request)
+
+		var got []entities.Task
+		err := json.NewDecoder(response.Body).Decode(&got)
+		if err != nil {
+			log.Fatal("JSON decoding failed")
+		}
+		want := 0
+		if len(got) != want {
+			t.Errorf("response is wrong, expected %d searched tasks but got %d tasks", want, len(got))
+		}
+	})
+}
